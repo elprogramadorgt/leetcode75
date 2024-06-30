e72b33efc9e5c14cff487461d817e83c1533b8da
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2024-06-30 17:06:02 -0500

    ++ Print function name and format convertions for 2 decimals

--- tests_ui/test.py
+++ tests_ui/test.py
@@ -13,10 +13,12 @@ def test_performance(func, *args):
     current, peak = tracemalloc.get_traced_memory()  # Get memory usage
     tracemalloc.stop()  # Stop tracing memory allocations
 
-    print(f"Execution Time: {elapsed_time} seconds")
-    print(f"Initial Memory: {current} bytes")
-    print(f"Final Memory: {peak} bytes")
-    print(f"Memory Usage: {peak-current} bytes")
+    print(f"Function Name: {func.__name__}\n")
+    print(f"Execution Time: {elapsed_time *1000:.2f} ms")
+    print(f"Initial Memory: {current/1024:.2f} MB")
+    print(f"Final Memory: {peak/1024:.2f} MB")
+    print(f"Memory Usage: {(peak-current)/1024:.2f} MB \n\n")
+  
 
     return elapsed_time, current, peak, peak-current
 
